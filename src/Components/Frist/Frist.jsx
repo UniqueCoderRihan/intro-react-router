ee20249896321639386e5cb45c599b32ab68711e
Rihanul Islam <rihanthedev@gmail.com>
2023-04-03 23:54:20 +0600

    nab,linkTo,usedata,loader

---
+++ src/Components/Frist/Frist.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Frist = () => {
+    return (
+        <div>
+            <h3>This is Defut Page For The Website</h3>
+        </div>
+    );
+};
+
+export default Frist;
--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,35 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Header from './Components/Header/Header';
+import About from './Components/ABout/About';
+import Contact from './Components/Contact/Contact';
+import Frist from './Components/Frist/Frist';
+import Friends from './Components/Friends/Friends';
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element:<App></App>
-  },
-  {
-    path:'/about',
-    element:<div>Hi Iam Form About</div>
+    element:<App></App>,
+    children: [
+      {
+        path:'/',
+        element: <Frist></Frist>
+      },
+      {
+        path:'/friends',
+        element: <Friends></Friends>,
+        loader:()=> fetch('https://jsonplaceholder.typicode.com/users')
+      },
+      {
+        path:'/about',
+        element: <About></About>
+      },
+      {
+        path:'/contact',
+        element: <Contact></Contact>
+      }
+    ]
   }
 
 ])
@@ -22,5 +42,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
         <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
