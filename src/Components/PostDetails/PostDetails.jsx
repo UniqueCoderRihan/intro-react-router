222eda0cc437d2c2baa3ee069bad3cd7c63afe60
Rihanul Islam <rihanthedev@gmail.com>
2023-04-04 01:39:59 +0600

    navigate go back

--- src/Components/PostDetails/PostDetails.jsx
+++ src/Components/PostDetails/PostDetails.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import './PostDetails.css'
 
 const PostDetails = () => {
     const details = useLoaderData();
     console.log(details);
     const {title,body,id} = details;
+    const navigateGoBack = useNavigate();
+    const handalGoBack = ()=>{
+        navigateGoBack(-1)
+    }
     return (
         <div className='postDetails'>
             <h2>About Post details Here </h2>
             <h3>Post Id: {id}</h3>
             <h4>{title}</h4>
             <p>{body}</p>
+            <button onClick={handalGoBack}>Go Back</button>
         </div>
     );
 };
