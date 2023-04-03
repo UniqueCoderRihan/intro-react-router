597eebe34efc52b6b03e5c393758efac708b015e
Rihanul Islam <rihanthedev@gmail.com>
2023-04-04 00:39:35 +0600

    details show with dynamic

--- src/Components/FriendDetail/FriendDetail.jsx
+++ src/Components/FriendDetail/FriendDetail.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const FriendDetail = () => {
+    const friendDetails = useLoaderData();
+    console.log(friendDetails);
+    const {name,id,username,email,phone,website} = friendDetails;
     return (
         <div>
             <h2>EveryThing Here To Gose</h2>
+            <h4>Name: {name}</h4>
+            <p>email: {email}</p>
+            <p>phone: {phone}</p>
+            <p><a href={website}>WebSite Click Here</a> </p>
         </div>
     );
 };
