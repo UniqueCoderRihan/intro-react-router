43851faaf1cbab02384d945790578c84edd617af
Rihanul Islam <rihanthedev@gmail.com>
2023-04-04 01:33:16 +0600

    btn handlaer on Navigate

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Post.css'
 
 const Post = ({post}) => {
     // console.log(post);  
+    const navigate = useNavigate();
     const {title,body,id} = post;
+
+    const handaleNavigate = ()=>{
+        navigate(`/post/${id}`)
+    }
     return (
         <div className='post'>
             <h3>Post Number {id}</h3>
             <p>{title}</p>
             <Link to={`/post/${id}`}> Show More Details</Link>
             <button>Show More Details</button>
+            <button onClick={handaleNavigate}>Btn handlar</button>
         </div>
     );
 };
