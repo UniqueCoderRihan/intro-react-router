2270f0d6f4777f9315a7093518719e50dad051fb
Rihanul Islam <rihanthedev@gmail.com>
2023-04-04 02:12:23 +0600

    error page setup

--- src/main.jsx
+++ src/main.jsx
@@ -51,6 +51,10 @@ const router = createBrowserRouter([
       {
         path:'/contact',
         element: <Contact></Contact>
+      },
+      {
+        path: '*',
+        element: <div>Hi</div>
       }
     ]
   }
